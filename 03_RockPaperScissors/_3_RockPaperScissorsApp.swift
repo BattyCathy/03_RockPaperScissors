d9d8259b1abcce1c5c343e1207a087764c74b6fa
Mackenzie Overman <39047747+BattyCathy@users.noreply.github.com>
2021-01-28 16:53:27 -0800

    Initial Commit

---
+++ 03_RockPaperScissors/_3_RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  _3_RockPaperScissorsApp.swift
+//  03_RockPaperScissors
+//
+//  Created by Austin Roach on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct _3_RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
